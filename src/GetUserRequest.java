0e820cbe9a13f8d69ac4693a0fea1fda2d0b2259
Isaac Reinaldo <isaacrpl7@hotmail.com>
2022-10-03 19:31:26 -0300

    Consumindo a API e tratando ela para mostrar no terminal

---
+++ src/GetUserRequest.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class GetUserRequest {
+    HttpResponse<String> response;
+    HttpRequest request;
+    public GetUserRequest(String url, String x_rapid_api_key, String x_rapid_api_host) {
+        this.request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("X-RapidAPI-Key", x_rapid_api_key)
+                .header("X-RapidAPI-Host", x_rapid_api_host)
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+    }
+
+    HttpResponse<String> executeRequest() {
+        try {
+            this.response = HttpClient.newHttpClient().send(this.request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return this.response;
+    }
+
+    String getStringResponse() {
+        return this.response.body();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GetUserRequest getUserRequest = new GetUserRequest("https://random-user.p.rapidapi.com/getuser",
+    "0d6178b922msh48f80de9c65c23ap1bbda8jsne622c61f187e",
+    "random-user.p.rapidapi.com");
+
+        getUserRequest.executeRequest();
+        String response = getUserRequest.getStringResponse();
+
+        UserAPIProcessor processor = new UserAPIProcessor();
+        processor.printUserInformation(response);
     }
 }
