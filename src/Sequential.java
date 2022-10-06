9a45ab83f7048000e484065f61413aef5bcc4135
Oscar Kemuel <oscar.kemuel5@gmail.com>
2022-10-06 14:08:16 -0300

    Ajusta API e printa tempo de execução para testes

--- src/Sequential.java
+++ src/Sequential.java
@@ -1,8 +1,10 @@
 public class Sequential {
 
     static void executarSequencial(int n) {
+        long initialTime = System.currentTimeMillis();
+
         for(int i = 0;i < n;i++) {
-            MakeRequest apiRequest = new MakeRequest("https://catfact.ninja/fact");
+            MakeRequest apiRequest = new MakeRequest("https://www.boredapi.com/api/activity");
 
             apiRequest.executeRequest();
             String response = apiRequest.getStringResponse();
@@ -10,9 +12,13 @@ public class Sequential {
             APIJSONProcessor processor = new APIJSONProcessor();
             processor.printApiInformation(response);
         }
+
+        long finalTime = System.currentTimeMillis();
+
+        System.out.println( "Runtime (ms): " + (finalTime - initialTime) + "ms" );
     }
 
     public static void main(String[] args) {
-        executarSequencial(10);
+        executarSequencial(40);
     }
 }
